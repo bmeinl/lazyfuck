b0ea795cb8141301c0dafd1de9ef34c8ef6b5c9f
Benjamin Meinl <b.meinl@gmail.com>
2010-08-29 02:38:59 +0200

    Added parser.

--- lazyfuck.hs
+++ lazyfuck.hs
@@ -1,9 +1,23 @@
 module Main where
 import Prelude hiding (lex)
+import Text.ParserCombinators.Parsec hiding (parse)
+import qualified Text.ParserCombinators.Parsec as P (parse)
 
-lex :: String -> [Char]
+data Expr = Op Char | Loop [Expr] deriving (Show)
+
+lex :: String -> String
 lex [] = []
 lex (x:xs)
     | x `elem` "<>+-,.[]" = x : lex xs
     | otherwise = lex xs
 
+parse = P.parse expr "" . lex
+
+expr :: Parser [Expr]
+expr = many (op <|> loop)
+
+loop :: Parser Expr
+loop = between (char '[') (char ']') expr >>= (return . Loop)
+
+op :: Parser Expr
+op = noneOf "[]" >>= (return . Op)
